f6b846c238f04a488fa621b3d3ad589060febe60
Eric Hansson <erichansson.united@gmail.com>
2024-01-06 20:41:18 +0000

    Have worked in implementing the checking of rows, colums, and cross for checking who won the game. Then I worked on small prompt setups.

--- main.cpp
+++ main.cpp
@@ -16,9 +16,11 @@ using namespace std;
 
 // Size of the grid
 #define GRIDSIZE 3
+// Total of moves that can be taken
+int totalMoves = GRIDSIZE*GRIDSIZE;
 
 // Declare the grid
-string grid[GRIDSIZE][GRIDSIZE];
+char grid[GRIDSIZE][GRIDSIZE];
 
 // i is row, j is column
 void initGrid(){
@@ -58,6 +60,51 @@ bool playerStatus = false;
 // false = not over
 bool gameStatus = false;
 
+// function to check if there is a row of either X or O
+bool checkRow(char grid[][GRIDSIZE]){
+    for (int i=0; i < GRIDSIZE; i++) {
+        // loop through each row, check if the row has all X or O
+        if (grid[i][0] == grid[i][1] && grid[i][1] == grid[i][2]) {
+            return true; 
+        }
+    }
+    return false;
+}
+
+// function to check columns
+bool checkColumn(char grid[][GRIDSIZE]){
+    for (int i=0; i < GRIDSIZE; i++) {
+        if (grid[0][i] == grid[1][i] && grid[1][i] == grid[2][i]) {
+            return  true; 
+        } 
+    }
+    return false;
+}
+
+// function to check crossed
+bool checkCross(char grid[][GRIDSIZE]){
+    if (grid[0][0] == grid[1][1] && grid[1][1] == grid[2][2]) {
+        return true; 
+    } 
+    else if (grid[0][2] == grid[1][1] && grid[1][1] == grid[2][0]) {
+        return true; 
+    }
+    return false;
+}
+
+// fucntion to check if there is a row, column, or diagonal, game over
+bool gameOver(char grid[][GRIDSIZE]){
+    return (checkRow(grid) || checkCross(grid) || checkColumn(grid));
+}
+
+// function to declare the winner
+void winnerPrompt(){
+}
+
+// function to prompt a draw
+void drawPrompt(){
+}
+
 // player 1 is X, player 2 is O
 
 int main(int argc, char *argv[]){
@@ -70,8 +117,9 @@ int main(int argc, char *argv[]){
     cout << "Welcome to Tic-Tac-Toe!" << endl;
     cout << endl;
     
+    // Selection of player, and name input
     while (!playerStatus) {
-        cout << "Select Player 1 or Player 2: ";
+        cout << "Select Player 1 (X) or Player 2 (O): ";
         cin >> playerNum;
         if(playerNum == "1"){
            cout << "Player 1 please enter in your name: ";
@@ -94,11 +142,28 @@ int main(int argc, char *argv[]){
 
     // Initialize grid
     initGrid();
+    //printGrid();
+    cout << endl;
 
-    while (!gameStatus) { 
+    int movesMade = 0;
+
+    // The game
+    while (!gameOver(grid) || movesMade != totalMoves) { 
+        int row, col;
+        printGrid();
+        cout << playerOne << " please enter cordinate to place X (row, col): "; 
+        cin >> row;
+        cin >> col;
+        grid[row][col] = VAR_X;
+        cout << endl;
+        
+        printGrid();
+        cout << playerTwo << " please enter cordinate to place O (row, col): "; 
+        cin >> row;
+        cin >> col;
+        grid[row][col] = VAR_O;
+        cout << endl;
     }
-
-
 
     return 0;
 }
